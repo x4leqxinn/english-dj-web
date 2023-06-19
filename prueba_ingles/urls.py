7f6e1c917696f81fbe15195c3ecb31d42cca69b9
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-19 06:17:19 -0400

    DELETE: Bad ref

--- prueba_ingles/urls.py
+++ prueba_ingles/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('web_prueba_ingles.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
+
 ]
 
